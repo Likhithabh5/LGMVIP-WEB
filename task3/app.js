d906696e6ec053dc05d273d6a3e5e6b4eb01c05a
Likhithabh5 <likhithabh8@gmail.com>
2022-06-29 14:31:31 +0530

    Modified

--- task3/app.js
+++ task3/app.js
@@ -1,28 +1,170 @@
-var entry = document.getElementById("entry");
-entry.addEventListener("click",displayDetails);
+var form = document.querySelector("#userForm");
+const allUsersData = [];
 
-var row=1;
+// ------------------function to reset the form------------------
+const resetForm = function () {
+  form.classList.remove('was-validated')
+  const name = document.getElementById('name');
+  name.value = "";
 
-function displayDetails() {
-    var name=document.getElementById("name").value;
-    var email=document.getElementById("email").value;
-    var websitelink=document.getElementById("websitelink").value;
-    var img=document.getElementById("img").value;
-    var gender=document.getElementById("gender").value;
-    var skills=document.getElementById("skills").value;
+  const email = document.getElementById('email');
+  email.value = "";
 
-    if(!name || !email || !websitelink || !img || !gender || !skills ){
-        alert("Please fill all the detials");
-        return;
+  const website = document.getElementById('website');
+  website.value = "";
+
+  const image = document.getElementById('image');
+  image.value = "";
+
+  const genderEl = document.querySelectorAll('input[name="gender"]');
+  for (const rb of genderEl) {
+    rb.checked = false;
+  }
+
+  const skillEl = document.querySelectorAll('input[name="skill"]');
+  for (const rb of skillEl) {
+    rb.checked = false;
+  }
+};
+
+// --------------------function to get the data of the form----------------------
+
+const getData = function () {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const website = document.getElementById('website').value;
+  const image = document.getElementById('image').value;
+  let gender;
+  let skills = [];
+
+  const genderEl = document.querySelectorAll('input[name="gender"]');
+  for (const rb of genderEl) {
+    if (rb.checked) {
+      gender = rb.value;
+      break;
     }
-    var display=document.getElementById("display");
+  };
 
-    var newRow=display.insertRow(row);
+  const skillEl = document.querySelectorAll('input[name="skill"]');
+  for (const rb of skillEl) {
+    if (rb.checked) {
+      skills.push(rb.value);
+    }
+  }
+  return { name, email, website, image, gender, skills };
+};
 
-    var cell1=newRow.insertCell(0);
-    var cell2=newRow.insertCell(1);
-    var cell3=newRow.insertCell(2);
+//-----------------------adding event listner to the "enroll student" button with type submit to submit the form
 
-    cell1.innerHTML=name;
-    cell2.innerHTML=
-}
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (form.checkValidity()) {
+    const data = getData();
+    allUsersData.push(data);
+    printResult(data);
+    resetForm();
+
+
+  } else {
+    form.classList.add('was-validated');
+  };
+  removeSpan();
+});
+
+// --------------function to remove the span tag ("fill the form to enroll the students")
+
+function removeSpan() {
+  var span = document.getElementById("span");
+  if(span){
+    span.remove();
+  }
+  
+};
+
+// ------------------function to print the form data in the right side of div by genrating html elments inside the div.
+
+function printResult(data) {
+  const resultEl = document.getElementById('enrolled-students');
+  let sectionHeading = null;
+  if (allUsersData.length == 1) {
+
+    sectionHeading = document.createElement('div');
+    const description = document.createElement('p');
+    description.innerHTML = "Description";
+    description.className = "description";
+
+    const image = document.createElement('p');
+    image.innerHTML = "Image"
+    image.className = "Image";
+
+    sectionHeading.className = "sectionHeading";
+    sectionHeading.append(description, image);
+  };
+
+  const wrapper = document.createElement('div');
+  wrapper.className = "wrapper";
+  wrapper.addEventListener('click', function (e) {
+    console.log(e.target.className);
+    if (e.target.className.includes('userDeleteBtn')) {
+      console.log('aaadfasdfasdf');
+      e.currentTarget.remove();
+    }
+
+  });
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = "+";
+  deleteBtn.className = "userDeleteBtn";
+
+  const textInfoContainer = document.createElement('div');
+  textInfoContainer.className = "textInfoContainer";
+
+  const imageContainer = document.createElement('div');
+  imageContainer.className = "imageContainer";
+
+  const imageHyperlink = document.createElement('a');
+  imageHyperlink.href = data.image;
+  imageHyperlink.target = "_blank";
+
+
+  let name = document.createElement('p');
+  name.className = "infoText userName";
+  name.innerHTML = data.name;
+
+  let gender = document.createElement('p');
+  gender.className = "infoText gender";
+  gender.innerHTML = data.gender;
+
+  let email = document.createElement('p');
+  email.className = "infoText email";
+  email.innerHTML = data.email;
+
+  let website = document.createElement('a');
+  website.className = "infoText website";
+  website.innerHTML = data.website;
+  website.href = data.website;
+  website.target = "_blank";
+
+  let skills = document.createElement('p');
+  skills.className = "infoText skills";
+  skills.innerHTML = data.skills.join(', ');
+
+
+  let userImage = document.createElement('img');
+  userImage.className = "userImage";
+  userImage.src = data.image;
+
+
+  textInfoContainer.append(name, gender, email, website, skills);
+  imageHyperlink.appendChild(userImage);
+  imageContainer.appendChild(imageHyperlink);
+
+  wrapper.append(textInfoContainer, imageContainer, deleteBtn);
+
+  if (sectionHeading == null) {
+    resultEl.append(wrapper);
+  } else {
+    resultEl.append(sectionHeading, wrapper)
+  };
+
+};
